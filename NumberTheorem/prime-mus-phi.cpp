377c0b1e4a5cc0d42ef0a265d5437ab4abddf8db
WIZeaz <764696642@qq.com>
2018-09-30 00:23:52 +0800

    Repairing function
    repair function

--- NumberTheorem/prime-mus-phi.cpp
+++ NumberTheorem/prime-mus-phi.cpp
@@ -1,26 +1,42 @@
-// 欧拉， 莫比乌斯函数， 质数线性筛
-const int N=100000;
-int prime[N+10]= {0},mus[N+10]= {0},num=0,phi[N+10]={0};
-// prime-质数 mus-莫比乌斯函数 phi-欧拉函数
-void pre() {
-	mus[1]=1;
-    phi[1]=0;
-	for(int i=2; i<=N; i++) {
-		if(!prime[i]){
-			prime[++num]=i;
-			mus[i]=-1;
-			phi[i]=i-1;
-		}
-		for(int o=1; o<=num&&i<=N/prime[o]; o++) {
-			prime[i*prime[o]]++;
-			if(i%prime[o]==0){
-				phi[i*prime[o]]=phi[i]*prime[o];
-				break;
-			}
-			else {
-				mus[i*prime[o]]=-mus[i];
-				phi[i*prime[o]]=phi[i]*(prime[o]-1);
-			}
-		}
-	}
+// 欧拉，莫比乌斯函数，质数线性筛 
+// prime-质数 mo-莫比乌斯函数 phi-欧拉函数 primeList-质数表
+#include <iostream>
+#include <cstring>
+using namespace std;
+#define N 100000
+int primeList[N+1];
+bool isPrime[N+1];
+int mo[N+1];
+int phi[N+1];
+int num;
+void Sieve(int n)
+{
+    num=0;
+    memset(isPrime,true,sizeof(isPrime));
+    phi[1]=1;
+	mo[1]=1;
+    for (int i=2;i<=n;++i){
+        if (isPrime[i]) {
+            primeList[num]=i;
+            mus[i]=-1;
+            phi[i]=i-1;
+            ++num;
+        }
+        for (int j=0;j<num && primeList[j]*i<=n;++j){
+            isPrime[primeList[j]*i]=false;
+            if (i%primeList[j]==0){
+                mo[primeList[j]*i]=0;
+                phi[primeList[j]*i]=phi[i]*primeList[j];
+                break;
+            }
+            else{
+                phi[primeList[j]*i]=phi[primeList[j]]*phi[i];
+                mo[primeList[j]*i]=-mo[i];
+            }
+        }
+    }
 }
+int main()
+{
+}
+
