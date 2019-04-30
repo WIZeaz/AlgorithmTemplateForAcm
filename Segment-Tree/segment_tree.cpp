eff49803fcb5f39a73759530e9977470edf1f5a1
WIZeaz <34786011+WIZeaz@users.noreply.github.com>
2019-05-01 01:13:48 +0800

    optimize

--- Segment-Tree/segment_tree.cpp
+++ Segment-Tree/segment_tree.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 using namespace std;
-struct node{
-    int l,r,lc,rc;
-    int val;
-};
-const int N=100000;
-const int root=0;
 struct Segment_Tree{
-    //constants
+    //constants and innerclass
+    struct node{
+        int l,r,lc,rc;
+        int val,cover;
+    };
+    static const int N=100000;
+    static const int root=0;
 
-    //clarification
+    //variables
     int num;    
     node tr[2*N];
 
@@ -29,36 +30,58 @@ struct Segment_Tree{
             build(mid+1,r,tr[now].rc);
         }
     }
+    void lazyupdate(node &x, int increment){
+        //set flag and update
+        x.cover+=increment;
+        x.val+=increment*(x.r-x.l+1);
+    }
+    void pushdown(node &f,node &lc, node &rc){
+        //transfer flag and update
+        lazyupdate(lc,f.cover);
+        lazyupdate(rc,f.cover);
+        f.cover=0;
+    }
+    void pushup(node &f,node &lc,node &rc){
+        f.val=lc.val+rc.val;
+    }
+
+    //warning: don't invoke both update and updateR in single segment tree, it may cause error
     void update(int pos,int val,int now=root){
         int mid=(tr[now].l+tr[now].r)>>1;
-        if (tr[now].l==pos && tr[now].r==pos) {
-            //change the value you need
+        if (tr[now].l==tr[now].r) {
             tr[now].val+=val; 
             return;
         } else if (pos<=mid) update(pos,val,tr[now].lc);
         else update(pos,val,tr[now].rc);
         //write parent update here
-        tr[now].val=tr[tr[now].lc].val+tr[tr[now].rc].val;
+        pushup(tr[now],tr[tr[now].lc],tr[tr[now].rc]);
     }
-    /*void update(int l,int r,int val,int now=root){
+
+    void updateR(int l,int r,int val,int now=root){
         
         if (tr[now].l==l && tr[now].r==r) {
             //lazy operation
-            tr[now].val=val; 
+            lazyupdate(tr[now],val);
             return;
         } else {
             //Pushdown here
-            //Pushdown end
+            if (tr[now].cover!=0) pushdown(tr[now],tr[tr[now].lc],tr[tr[now].rc]);
+
             int mid=(tr[now].l+tr[now].r)>>1;
-            if (pos<=mid) update(pos,val,tr[now].lc);
-            else update(pos,val,tr[now].rc);
+            if (r<=mid) updateR(l,r,val,tr[now].lc);
+            else if (l>mid) updateR(l,r,val,tr[now].rc);
+            else {
+                updateR(l,mid,val,tr[now].lc);
+                updateR(mid+1,r,val,tr[now].rc);
+            }
             //write parent update here
-            tr[now].val=tr[tr[now].lc].val+tr[tr[now].rc].val;
+            pushup(tr[now],tr[tr[now].lc],tr[tr[now].rc]);
         }
-    }*/
+    }
     int query(int l,int r,int now=root){
         if (tr[now].l==l && tr[now].r==r) return tr[now].val;
         else {
+            if (tr[now].cover!=0) pushdown(tr[now],tr[tr[now].lc],tr[tr[now].rc]);
             int mid=(tr[now].l+tr[now].r)>>1;
             if (r<=mid) return query(l,r,tr[now].lc);
             else if (l>mid) return query(l,r,tr[now].rc);
@@ -70,30 +93,19 @@ struct Segment_Tree{
 Segment_Tree tr;
 int main()
 {
-    int T;
-    char str[100];
-    scanf("%d",&T);
-    for (int Case=1;Case<=T;++Case){
-        int n,a,b;
-        scanf("%d",&n);
-        tr.setRange(1,n);
-        for (int i=1;i<=n;++i){
-            int tmp;
-            scanf("%d",&tmp);
-            tr.update(i,tmp);
-        }
-        printf("Case %d:\n",Case);
-        while (1){
-            scanf("%s",str);
-            if (strcmp(str,"End")==0) break;
-            scanf("%d%d",&a,&b);
-            if (strcmp(str,"Add")==0){
-                tr.update(a,b);
-            } else if (strcmp(str,"Sub")==0){
-                tr.update(a,-b);
-            } else {
-                printf("%d\n",tr.query(a,b));
-            }
+    int l,r;
+    cout<<"input range of segment tree: ";
+    cin>>l>>r;
+    tr.setRange(l,r);
+    cout<<"supported operations: \nADD l r val\nQUERY l r\n";
+    string str;
+    int val;
+    while (cin>>str>>l>>r){
+        if (str=="add"){
+            cin>>val;
+            tr.updateR(l,r,val);
+        } else if (str=="query"){
+            cout<<tr.query(l,r)<<endl;
         }
     }
 }
