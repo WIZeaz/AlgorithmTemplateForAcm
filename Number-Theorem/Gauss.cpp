3a552d01ea99975a4a0c2e245d5f89cc0d5b9525
Zhihao Li <43445615+lzh12139@users.noreply.github.com>
2019-05-15 14:55:41 +0800

    Create Gauss.cpp

---
+++ Number-Theorem/Gauss.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+// 高斯消元，从网上copy的板子
+// 用来找规律
+#define N 205
+using namespace std;
+const double eps=1e-8;
+int n;
+double a[N][N],del;
+bool gauss() {
+	for(int i=1; i<=n; i++) {
+		int k=i;
+		for(int j=i+1; j<=n; j++)
+			if(fabs(a[j][i])>fabs(a[k][i]))k=j;
+		if(fabs(del=a[k][i])<eps)return 0;
+		for(int j=i; j<=n+1; j++)swap(a[i][j],a[k][j]);
+		for(int j=i; j<=n+1; j++)a[i][j]/=del;
+		for(k=1; k<=n; k++)
+			if(k!=i) {
+				del=a[k][i];
+				for(int j=i; j<=n+1; j++)a[k][j]-=a[i][j]*del;
+			}
+	}
+	return 1;
+}
+int main() {
+	scanf("%d",&n);
+	for(int i=1; i<=n; i++)
+		for(int j=1; j<=n+1; j++)scanf("%lf",&a[i][j]);
+	bool flag=gauss();
+	if(!flag)puts("No Solution");
+	else for(int i=1; i<=n; i++)printf("%.2lf\n",a[i][n+1]);
+	return 0;
+}
