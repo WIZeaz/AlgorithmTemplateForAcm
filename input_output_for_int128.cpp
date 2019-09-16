999344a4c045466b894ceb5ff16573ca5b3ec754
WIZeaz <764696642@qq.com>
2019-09-16 16:28:22 +0800

    fix a bug in __int128.cpp

--- input_output_for_int128.cpp
+++ input_output_for_int128.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 inline __int128_t read()
 {
-   int X=0,w=0; char ch=0;
-   while(!isdigit(ch)) {w|=ch=='-';ch=getchar();}
-   while(isdigit(ch)) X=(X<<3)+(X<<1)+(ch^48),ch=getchar();
-   return w?-X:X;
+   __int128_t rnt=0;
+   int sign=0; 
+   char ch=0;
+   while(!isdigit(ch)) {sign|=ch=='-';ch=getchar();}
+   while(isdigit(ch)) rnt=(rnt<<3)+(rnt<<1)+(ch^48),ch=getchar();
+   return sign?-rnt:rnt;
 }
 inline void print(__int128_t x)
 {    
