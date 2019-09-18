7924446a9ff3c0c441e33c71a547c2846f79b140
WIZeaz <764696642@qq.com>
2019-09-18 18:10:46 +0800

    modify Gauss

--- Number-Theorem/Gauss.cpp
+++ Number-Theorem/Gauss.cpp
@@ -4,16 +4,15 @@
 #define N 205
 using namespace std;
 const double eps=1e-8;
-int n;
 double a[N][N],del;
-bool gauss() {
+bool gauss(int n) {
 	for(int i=1; i<=n; i++) {
 		int k=i;
 		for(int j=i+1; j<=n; j++)
 			if(fabs(a[j][i])>fabs(a[k][i]))k=j;
-		if(fabs(del=a[k][i])<eps)return 0;
-		for(int j=i; j<=n+1; j++)swap(a[i][j],a[k][j]);
-		for(int j=i; j<=n+1; j++)a[i][j]/=del;
+		if(fabs(del=a[k][i])<eps) return 0;
+		if (i!=k) for(int j=i; j<=n+1; j++) swap(a[i][j],a[k][j]);
+		for(int j=i; j<=n+1; j++) a[i][j]/=del;
 		for(k=1; k<=n; k++)
 			if(k!=i) {
 				del=a[k][i];
@@ -23,11 +22,11 @@ bool gauss() {
 	return 1;
 }
 int main() {
+	int n;
 	scanf("%d",&n);
 	for(int i=1; i<=n; i++)
 		for(int j=1; j<=n+1; j++)scanf("%lf",&a[i][j]);
-	bool flag=gauss();
-	if(!flag)puts("No Solution");
+	if(!gauss(n)) puts("No Solution");
 	else for(int i=1; i<=n; i++)printf("%.2lf\n",a[i][n+1]);
 	return 0;
 }
