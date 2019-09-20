73a99a929f10167e2fd3531dae9386ac07cc5964
Zhihao Li <43445615+lzh12139@users.noreply.github.com>
2019-09-20 17:15:29 +0800

    reformat

--- Number-Theorem/欧拉降幂.cpp
+++ Number-Theorem/欧拉降幂.cpp
@@ -1,27 +1,7 @@
-/**
- * codeforces-906D
-**/
+//codeforces-906D
 #include<bits/stdc++.h>
 using namespace std;
-#define mem(x, y) memset(x, (y), sizeof x)
-#define ff first
-#define ss second
-#define mp(x, y) make_pair(x, y)
-#define pb(x) emplace_back(x)
-#define sc(a) scanf("%d", &a)
-#define scl(a) scanf("%lld", &a)
-#define scs(a) scanf("%s", a)
-#define pr(a, b) printf("%d%c", a, b)
-#define prl(a, b) printf("%ll%c", a, b)
-#define all(v) v.begin(), v.end()
-#define IOS ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
-#define _yes printf("Yes\n")
-#define _no printf("No\n")
-
 typedef long long ll;
-typedef vector<int> V;
-typedef pair<int, int> pii;
-typedef map<int, int> mii;
 
 ll MOD(ll x,ll m){return x>=m?x%m+m:x;}
 ll quickmod(ll base,ll b,ll mod){
