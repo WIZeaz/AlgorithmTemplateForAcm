039a03820c7156864c0071f9759f6850d53a8845
lzh12139 <1270855077@qq.com>
2019-09-28 13:03:38 +0800

    fix a bug AND reformat

---
+++ Computational-Geometry/ConvexHull.cpp
@@ -0,0 +1,75 @@
+// 凸包，O(nlogn)
+// 输入点跟点的个数，输出凸包点的个数
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const double PI = acos(-1.0);
+const double eps = 1e-8;
+const int N = 50000;
+
+inline int sgn(double x){
+	if (fabs(x) < eps) return 0;
+	if (x < 0) return -1;
+	return 1;
+}
+
+struct Point{
+	double x, y;
+	Point(){};
+	Point(double _x, double _y) : x(_x), y(_y){};
+	Point operator+(const Point &a) const{
+		return Point(x + a.x, y + a.y);
+	}
+	Point operator-(const Point &a) const{
+		return Point(x - a.x, y - a.y);
+	}
+	double operator*(const Point &a) const{
+		return x * a.x + y * a.y;
+	}
+	double operator^(const Point &a) const{
+		return x * a.y - y * a.x;
+	}
+};
+
+double dist(Point a, Point b){
+	return sqrt((a - b) * (a - b));
+}
+
+Point a[N + 10], stack[N + 10];
+
+bool cmp(Point b, Point c){
+	int cmp = sgn((b - a[1]) ^ (c - a[1]));
+	if (cmp == 1) return true;
+	if (cmp == 0 && sgn(dist(b, a[1]) - dist(c, a[1])) == -1) return true;
+	return false;
+}
+
+int Graphm(Point a[], int n) {
+	int k = 1, num;
+	for (int i = 2; i <= n; i++)
+		if (sgn(a[i].y - a[k].y) == -1 || (sgn(a[i].y - a[k].y) == 0 && sgn(a[i].x - a[k].x) == -1))
+			k = i;
+
+	swap(a[1], a[k]);
+	sort(a + 2, a + 1 + n, cmp);
+
+	if (n == 1){
+		stack[1] = a[1];
+		return 1;
+	}
+	else if (n == 2){
+		stack[1] = a[1], stack[2] = a[2];
+		return 2;
+	}
+	else{
+		stack[1] = a[1], stack[2] = a[2], num=2;
+		for (int i = 3; i <= n; i++) {
+			while (num > 1 && sgn((stack[num] - stack[num - 1]) ^ (a[i] - stack[num - 1])) <= 0)
+				num--;
+			stack[++num] = a[i];
+		}
+		return num;
+	}
+}
