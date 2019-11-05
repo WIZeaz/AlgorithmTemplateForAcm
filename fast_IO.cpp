da47c6625ef760222e7784979443d8f85b4070bf
Zhihao Li <43445615+lzh12139@users.noreply.github.com>
2019-11-05 16:51:05 +0800

    upd FastIO,use fread and fwrite

--- fast_IO.cpp
+++ fast_IO.cpp
@@ -1,14 +1,53 @@
-template <class T>
-inline bool read(T &ret) {
-	char c; int sgn;
-	if(c=getchar(),c==EOF) return 0; //EOF 
-	while(c!='-'&&(c<'0'||c>'9')) c=getchar(); sgn=(c=='-')?-1:1;
-	ret=(c=='-')?0:(c-'0');
-	while(c=getchar(),c>='0'&&c<='9') ret=ret*10+(c-'0'); ret*=sgn;
-	return 1;
-}
-
-inline void out(long long x) {
-	if(x>9)out(x/10); 
-	putchar(x%10+'0');
-}
+struct FastIO {
+	static const int LEN=1<<18|1;
+	char buf[LEN],wbuf[LEN];
+	int s=0,t=0,wpos=0;
+	inline int read(){
+		return (s==t)&&(t=(s=0)+fread(buf,1,LEN,stdin)),s==t?-1:buf[s++];
+	}
+	inline void out(int x){
+		wpos==LEN?fwrite(wbuf,1,LEN,stdout),wpos=0:wbuf[wpos++]=x;
+	}
+	// 有符号：
+	inline int rint(){
+		// 根据读入是int还是long long决定x的类型
+		int c=read(),x=0,s=1;
+		if(c==-1)return 0;
+		while(c<=32)c=read();
+		if(c=='-')s=-1,c=read();
+		while(isdigit(c))x=x*10+c-'0',c=read();
+		return x*s;
+	}
+	inline int ruint(){
+		int c=read(),x=0;
+		while(c<=32)c=read();
+		while(isdigit(c))x=x*10+c-'0',c=read();
+		return x;
+	}
+	inline void rstr(char *str){
+		int c=read();
+		while(c<=32)c=read();
+		while(c>32)*str++=c,c=getchar();
+		*str=0;
+	}
+	// 根据类型
+	inline void wint(long long x){
+		// 如果含有负数，取消注释
+		// if(x<0)wchar('-'),x=-x;
+		char str[24];int n=0;
+		while(x||!n)str[n++]='0'+x%10,x/=10;
+		while(n--)out(str[n]);
+	}
+	inline void wchar(char str){
+		out(str);
+	}
+	inline void wstr(const char *s){
+		while(*s)out(*s++);
+	}
+	inline void over(){
+		if(wpos)fwrite(wbuf,1,wpos,stdout),wpos=0;
+	}
+	~FastIO(){
+		if(wpos)fwrite(wbuf,1,wpos,stdout),wpos=0;
+	}
+}io;
