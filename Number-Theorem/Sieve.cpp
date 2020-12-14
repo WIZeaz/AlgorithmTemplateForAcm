68b705d21a92089e398223028fb156820c004383
WIZeaz <764696642@qq.com>
2020-12-15 02:53:25 +0800

    add segmentSeive

--- Number-Theorem/Sieve.cpp
+++ Number-Theorem/Sieve.cpp
@@ -4,9 +4,10 @@ Linear Sieve
 */
 #include <iostream>
 #include <cstring>
+#include <vector>
 using namespace std;
-#define N 100000
-int primeList[N+1];
+#define N 10000000
+vector<int> primes;
 bool isPrime[N+1];
 int mo[N+1];
 int phi[N+1];
@@ -18,27 +19,43 @@ void Sieve(int n){
     mo[1]=1;
     for (int i=2;i<=n;++i){
         if (isPrime[i]) {
-            primeList[num]=i;
+            primes.push_back(i);
             mo[i]=-1;
             phi[i]=i-1;
-            ++num;
         }
-        for (int j=0;j<num && primeList[j]*i<=n;++j){
-            isPrime[primeList[j]*i]=false;
-            if (i%primeList[j]==0){
-                mo[primeList[j]*i]=0;
-                phi[primeList[j]*i]=phi[i]*primeList[j];
+        for (int j=0;j<primes.size() && primes[j]*i<=n;++j){
+            isPrime[primes[j]*i]=false;
+            if (i%primes[j]==0){
+                mo[primes[j]*i]=0;
+                phi[primes[j]*i]=phi[i]*primes[j];
                 break;
             }
             else{
-                phi[primeList[j]*i]=phi[primeList[j]]*phi[i];
-                mo[primeList[j]*i]=-mo[i];
+                phi[primes[j]*i]=phi[primes[j]]*phi[i];
+                mo[primes[j]*i]=-mo[i];
             }
         }
     }
 }
+
+//区间埃氏筛 效率O( (r-l)loglog(r-l) )
+bool segPrime[N];
+using ll=long long;
+ll SegmentSieve(ll l,ll r){
+    if (l<2) l=2;
+    ll ans=r-l+1;
+    for (ll i=l;i<=r;++i) segPrime[i-l]=true;
+    for (auto p:primes){
+        if (p*p>r) break;
+        for (ll i=max((l+p-1)/p,2ll)*p;i<=r;i+=p){
+            segPrime[i-l]=false;
+        }
+    }
+    return ans;
+}
 int main()
 {
-    Sieve(5000);
-    for (int i=0;primeList[i]<4000;++i) printf("%d ",primeList[i]);
+    Sieve(10000000);
+    cout<<num;
+    //for (int i=0;primeList[i]<4000;++i) printf("%d ",primeList[i]);
 }
